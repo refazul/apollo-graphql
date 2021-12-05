c514f683c83bf9c7f3109ce44c463d15def7170e
refazul <refazul.refat@yahoo.com>
2021-12-05 14:01:18 +0600

    pickbazar furniture shop

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import client from '../apollo-client';
+
+import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
